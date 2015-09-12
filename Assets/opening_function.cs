2b49abfe70d4c7b1f236ee721ee6590722ddb80e
pawnWizard <ericlar@umich.edu>
2015-09-12 03:20:51 -0400

    opening function generates one line

--- Assets/opening_function.cs
+++ Assets/opening_function.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class opening_function : MonoBehaviour {
-
+	
+	public Object line_object;
 	// Use this for initialization
 	void Start () {
+		Instantiate (line_object, new Vector3 (0, 0, 0), Quaternion.identity);
 	
 	}
 	
