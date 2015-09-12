0032bc8c598e99f816be9d708f99e6f2f6187fab
naibla8358 <naibla8358@gmail.com>
2015-09-12 05:00:22 -0400

    Cleaned up the opening_function a bit. Auto generates nodes now.

--- Assets/opening_function.cs
+++ Assets/opening_function.cs
@@ -1,54 +1,56 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class opening_function : MonoBehaviour {
-	
-	//public Object line_object;
-	public LineRenderer renderer;
+
+	public LineRenderer renderer_obj;
+
+	public GameObject node_obj;
 
 	// Use this for initialization
 	void Start () {
-		/*
-		Instantiate (line_object, new Vector3 (2, 0, 3), Quaternion.identity);
-		Instantiate (line_object, new Vector3 (2, 0, 2), Quaternion.Euler(0,90,0));
-		Instantiate (line_object, new Vector3 (2, 0, 2), Quaternion.identity);
-		Instantiate (line_object, new Vector3 (2, 0, 1), Quaternion.Euler(0,90,0));
-		Instantiate (line_object, new Vector3 (3, 0, 1), Quaternion.Euler(0,90,0));
-		*/
+		if (renderer_obj == null || node_obj == null)
+			throw new ArgumentNullException();
 
-		LineRenderer r = Instantiate(renderer) as LineRenderer;
-		r.SetVertexCount(2);
-		r.SetPosition(0, new Vector3 (2, 3, 0));
-		r.SetPosition(1, new Vector3 (3, 3, 0));
-		r.SetWidth (0.1f, 0.1f);
+		Vector3 posA = new Vector3(2, 3);
+		Vector3 posB = new Vector3(3, 3);
+		Vector3 posC = new Vector3(2,2);
+		Vector3 posD = new Vector3(3,2);
+		Vector3 posE = new Vector3(2,1);
+		Vector3 posF = new Vector3(3,1);
 
-		r = Instantiate(renderer);
-		r.SetVertexCount(2);
-		r.SetPosition(0, new Vector3 (2, 3, 0));
-		r.SetPosition(1, new Vector3 (2, 2, 0));
-		r.SetWidth (0.1f, 0.1f);
+		/*CreateLine(new Vector3(2,3), new Vector3(3,3));
+		CreateLine(new Vector3(2,3), new Vector3(2,2));
+		CreateLine(new Vector3(2,2), new Vector3(3,2));
+		CreateLine(new Vector3(2,2), new Vector3(2,1));
+		CreateLine(new Vector3(3,2), new Vector3(3,1));*/
 
-		r = Instantiate(renderer);
-		r.SetVertexCount(2);
-		r.SetPosition(0, new Vector3 (2, 2, 0));
-		r.SetPosition(1, new Vector3 (3, 2, 0));
-		r.SetWidth (0.1f, 0.1f);
+		CreateLine(posA, posB);
+		CreateLine(posA, posC);
+		CreateLine(posC, posD);
+		CreateLine(posC, posE);
+		CreateLine(posD, posF);
 
-		r = Instantiate(renderer);
-		r.SetVertexCount(2);
-		r.SetPosition(0, new Vector3 (2, 2, 0));
-		r.SetPosition(1, new Vector3 (2, 1, 0));
-		r.SetWidth (0.1f, 0.1f);
+		CreateNode(posA);
+		CreateNode(posB);
+		CreateNode(posC);
+		CreateNode(posD);
+		CreateNode(posE);
+		CreateNode(posF);
+	}
 
-		r = Instantiate(renderer);
+	private void CreateLine(Vector3 start, Vector3 end)
+	{
+		LineRenderer r = Instantiate(renderer_obj);
 		r.SetVertexCount(2);
-		r.SetPosition(0, new Vector3 (3, 2, 0));
-		r.SetPosition(1, new Vector3 (3, 1, 0));
+		r.SetPosition(0, start);
+		r.SetPosition(1, end);
 		r.SetWidth (0.1f, 0.1f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void CreateNode(Vector3 position)
+	{
+		Instantiate(node_obj, position, Quaternion.identity);
 	}
 }
