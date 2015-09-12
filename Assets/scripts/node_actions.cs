22fe585f2cd58b321362cc860ad5a9db45b6021f
djrobins <dejoro@umich.edu>
2015-09-12 13:43:29 -0400

    Drag is Working!

--- Assets/scripts/node_actions.cs
+++ Assets/scripts/node_actions.cs
@@ -5,7 +5,12 @@ using System.Collections;
 
 public class node_actions : MonoBehaviour
 {
+
 	public event Action Clicked;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private float lockedYPosition;
+	public static Vector3 scanPos;
 
 	void BeginConnectScript(object sender)
 	{
@@ -18,6 +23,24 @@ public class node_actions : MonoBehaviour
 		//Clear out the event to prevent issues
 		Clicked = null;
 	}
-	
+
+	void OnMouseDown() {
+		//Causes centering. Delete, maybe?
+		//screenPoint = Camera.main.WorldToScreenPoint (scanPos);
+		lockedYPosition = screenPoint.y;
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+			                                             Input.mousePosition.y, screenPoint.z));
+		Cursor.visible = false;
+	}
+			                                
+	void OnMouseDrag() {
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;				
+	}
+
+	void OnMouseUp() {
+		Cursor.visible = true;
+	}
 }
 
